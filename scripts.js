22551c63d6de75ee4587ca45261c66bfe1f020f3
varunsahuclinic <varunforphd@gmail.com>
2024-07-18 15:55:41 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Thank you for contacting us! We will get back to you soon.');
-});
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
